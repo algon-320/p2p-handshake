4e7b33ac5b9503d58aded2850e8a702c11b3f4ac
algon-320 <algon.0320@gmail.com>
2021-08-13 05:24:28 +0900

    Fix UDP packet length issue

--- src/message.rs
+++ src/message.rs
@@ -35,9 +35,17 @@ pub fn send_to<T: Serialize>(v: T, sock: &UdpSocket, to: SocketAddr) -> Result<(
 }
 
 pub fn recv_from<T: serde::de::DeserializeOwned>(sock: &UdpSocket) -> Result<(T, SocketAddr)> {
-    let mut buf = [0; 4096];
-    let (sz, src) = sock.recv_from(&mut buf)?;
-    trace!("src={}", src);
-    let msg: T = serde_cbor::from_slice(&buf[..sz])?;
-    Ok((msg, src))
+    use std::cell::RefCell;
+    thread_local! {
+        static WORKING_BUF: RefCell<Vec<u8>> = RefCell::new(vec![0; 1 << 16]);
+    }
+
+    WORKING_BUF.with(|buf| {
+        let mut buf = buf.borrow_mut();
+        let buf = buf.as_mut_slice();
+        let (sz, src) = sock.recv_from(buf)?;
+        trace!("src={}", src);
+        let msg: T = serde_cbor::from_slice(&buf[..sz])?;
+        Ok((msg, src))
+    })
 }
